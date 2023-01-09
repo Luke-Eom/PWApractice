5391dd56cfa8bb7e8bfccf3d5417ce28985adc84
LukeEom <eluke1028@gmail.com>
2023-01-09 22:54:49 +0900

    trial

--- Kafka&PWA.js
+++ Kafka&PWA.js
@@ -5,4 +5,4 @@ console.log("Now", now())
 // 프로젝트 단위로 모듈 적용
 // package.json 파일에 json 형식으로
 // { "type" :  module""}
-// 그 후 확장자를 js로
+// 그 후 확장자를 js로';
--- do-it_PWA/arrowFunction.js
+++ do-it_PWA/arrowFunction.js
@@ -1,5 +1,13 @@
+
 /* => fat arrow
     : intuitive way to use function in javascripts
 
 
 */
+const fnAttdCheckIn = () => '{{ enterTime }}에 오셨습니다!' // datetime.now 지금 시간 
+
+const fnAttdCheckOut = () => '{{ leaveTime }}에 나가셨습니다!' // datetime.now 지금 시간
+
+const fnCalWOTime = (fnAttdCheckIn, fnAttdCheckOut) => fnAttdCheckOut() - fnAttdCheckIn();
+
+const fnWorkOutTime = () => '{{ fnCalWOTime결과값 }} 운동하셨습니다!'
